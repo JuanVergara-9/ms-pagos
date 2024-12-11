1445817e186081443d91a5e0cb5782db137af8f9
JuanVergara-9 <juanvergara920@gmail.com>
2024-12-10 23:01:46 -0300

    limit

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .extension import db, migrate
+from .extension import db, migrate, limiter
 from .routes import pagos
 
 def create_app():
@@ -8,6 +8,7 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+    limiter.init_app(app)
 
     app.register_blueprint(pagos)
 
--- app/routes.py
+++ app/routes.py
@@ -3,11 +3,13 @@ from tenacity import retry
 from tenacity.wait import wait_fixed
 from tenacity.stop import stop_after_attempt
 from .services import add_pagos_service, remove_pagos_service
+from .extension import limiter
 
 pagos = Blueprint('pagos', __name__)
 
 @pagos.route('/pagos/add', methods=['POST'])
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(0.5))
+@limiter.limit("5 per minute")  # Limitar a 5 solicitudes por minuto
 def add_pagos():
     data = request.get_json()
     response, status = add_pagos_service(data)
@@ -15,6 +17,7 @@ def add_pagos():
 
 @pagos.route('/pagos/remove', methods=['POST'])
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(0.5))
+@limiter.limit("5 per minute")  # Limitar a 5 solicitudes por minuto
 def remove_pagos():
     data = request.get_json()
     response, status = remove_pagos_service(data)
